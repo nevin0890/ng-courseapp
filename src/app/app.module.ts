1dac4a6bce5e7054ff3f6fadc545d23de0768c2c
nevin0890 <bcab19_2217@santhigiricollege.com>
2022-10-10 23:58:03 +0530

    add course added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddcourseComponent } from './addcourse/addcourse.component';
 
 const appRoutes: Routes = [
-  {
-    
-  }
+  { path: '', component: AddcourseComponent },
+
 ];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddcourseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
